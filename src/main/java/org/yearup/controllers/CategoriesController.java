e701222bccdc48ce0f2c26c965176264ed639873
ReveristRealm <djac6272@gmail.com>
2023-06-26 01:32:56 -0400

    90% done with Phase 1

--- src/main/java/org/yearup/controllers/CategoriesController.java
+++ src/main/java/org/yearup/controllers/CategoriesController.java
@@ -1,8 +1,8 @@
 package org.yearup.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
 import org.yearup.data.CategoryDao;
 import org.yearup.data.ProductDao;
 import org.yearup.models.Category;
@@ -10,30 +10,43 @@ import org.yearup.models.Product;
 
 import java.util.List;
 
-// add the annotations to make this a REST controller
-// add the annotation to make this controller the endpoint for the following url
+// add the annotations to make this a REST controller (DONE)
+// add the annotation to make this controller the endpoint for the following url (DONE)
     // http://localhost:8080/categories
-// add annotation to allow cross site origin requests
+// add annotation to allow cross site origin requests (DONE)
+@RestController
+@RequestMapping("/categories")
+@CrossOrigin
 public class CategoriesController
 {
+    @Autowired
     private CategoryDao categoryDao;
+    @Autowired
     private ProductDao productDao;
 
 
-    // create an Autowired controller to inject the categoryDao and ProductDao
+    // create an Autowired controller to inject the categoryDao and ProductDao (DONE)
 
-    // add the appropriate annotation for a get action
-    public List<Category> getAll()
-    {
-        // find and return all categories
-        return null;
+    public CategoriesController(CategoryDao catDao, ProductDao proDao)  {
+        this.categoryDao = catDao;
+        this.productDao = proDao;
     }
 
-    // add the appropriate annotation for a get action
+    // add the appropriate annotation for a get action (DONE)
+    @GetMapping("/categories")
+    public List<Category> getAll()
+    {
+        // find and return all categories(DONE)
+
+        return categoryDao.getAllCategories();
+    }
+
+    // add the appropriate annotation for a get action (DONE)
+    @GetMapping
     public Category getById(@PathVariable int id)
     {
-        // get the category by id
-        return null;
+
+        return categoryDao.getById(id);
     }
 
     // the url to return all products in category 1 would look like this
@@ -41,16 +54,18 @@ public class CategoriesController
     @GetMapping("{categoryId}/products")
     public List<Product> getProductsById(@PathVariable int categoryId)
     {
-        // get a list of product by categoryId
-        return null;
+        // get a list of product by categoryId (DONE)
+        return productDao.listByCategoryId(categoryId);
     }
 
-    // add annotation to call this method for a POST action
-    // add annotation to ensure that only an ADMIN can call this function
+    // add annotation to call this method for a POST action (DONE)
+    // add annotation to ensure that only an ADMIN can call this function (DONE)
+    @PostMapping()
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public Category addCategory(@RequestBody Category category)
     {
-        // insert the category
-        return null;
+        // insert the category (DONE)
+        return categoryDao.create(category);
     }
 
     // add annotation to call this method for a PUT (update) action - the url path must include the categoryId
@@ -58,13 +73,17 @@ public class CategoriesController
     public void updateCategory(@PathVariable int id, @RequestBody Category category)
     {
         // update the category by id
+
     }
 
 
-    // add annotation to call this method for a DELETE action - the url path must include the categoryId
-    // add annotation to ensure that only an ADMIN can call this function
+    // add annotation to call this method for a DELETE action - the url path must include the categoryId (DONE)
+    // add annotation to ensure that only an ADMIN can call this function (DONE)
+    @PostMapping("{categoryId}")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public void deleteCategory(@PathVariable int id)
     {
-        // delete the category by id
+        // delete the category by id (DONE)
+        categoryDao.delete(id);
     }
 }
--- src/main/java/org/yearup/data/mysql/MySqlCategoryDao.java
+++ src/main/java/org/yearup/data/mysql/MySqlCategoryDao.java
@@ -5,8 +5,9 @@ import org.yearup.data.CategoryDao;
 import org.yearup.models.Category;
 
 import javax.sql.DataSource;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import javax.xml.transform.Result;
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 @Component
@@ -20,34 +21,100 @@ public class MySqlCategoryDao extends MySqlDaoBase implements CategoryDao
     @Override
     public List<Category> getAllCategories()
     {
-        // get all categories
-        return null;
+        ArrayList<Category> allCat = new ArrayList<>();
+        String query = "SELECT * FROM categories;";
+
+        try(Connection connection = getConnection();
+            PreparedStatement stmt = connection.prepareStatement(query);
+            ResultSet results = stmt.executeQuery();
+        ){
+            while(results.next()){
+                int category_id = results.getInt("category_id");
+                String name = results.getString("name");
+                String description = results.getString("description");
+                Category category = new Category(category_id,name,description);
+                allCat.add(category);
+            }
+
+        }catch(Exception e){
+            System.out.println("hi");
+        }
+        return allCat;
     }
 
     @Override
     public Category getById(int categoryId)
     {
-        // get category by id
-        return null;
+        Category category = null;
+       String query = "SELECT * FROM categories WHERE category_id = ?;";
+       try(Connection connection = getConnection();
+           PreparedStatement stmt = connection.prepareStatement(query);
+       ){
+           stmt.setInt(1,categoryId);
+
+           try(ResultSet results = stmt.executeQuery()){
+               if (results.next()){
+                  category = mapRow(results);
+               }
+           }
+
+       }catch(Exception e){
+           System.out.println("hi");
+       }
+        return category;
     }
 
     @Override
     public Category create(Category category)
     {
-        // create a new category
-        return null;
+        String query = "INSERT INTO categories (category_id,name,description) VALUES (?,?,?);";
+        try (Connection connection = getConnection();
+             PreparedStatement stmt = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS)) {
+            stmt.setInt(1, category.getCategoryId());
+            stmt.setString(2, category.getName());
+            stmt.setString(3, category.getDescription());
+
+            int rows = stmt.executeUpdate();
+
+            if (rows == 0) {
+                throw new SQLException("Creating category failed, no rows affected.");
+            }
+            try (ResultSet generatedKeys = stmt.getGeneratedKeys()) {
+                if (generatedKeys.next()) {
+                    int key = generatedKeys.getInt(1);
+                    category.setCategoryId(key);
+                } else {
+                    throw new SQLException("Creating category failed.");
+                }
+
+            }
+            } catch (Exception e) {
+                System.out.println("Hi");
+            }
+
+            return category;
     }
 
     @Override
     public void update(int categoryId, Category category)
     {
-        // update category
+       String query ="UPDATE categories SET WHERE ";
     }
 
     @Override
     public void delete(int categoryId)
     {
-        // delete category
+       String query = "DELETE FROM categories WHERE category_id = ?";
+       try (Connection connection = getConnection();
+            PreparedStatement stmt = connection.prepareStatement(query);
+       ){
+
+        stmt.setInt(1,categoryId);
+
+        stmt.executeUpdate();
+       }catch(Exception e){
+           System.out.println("Whatever you did, didn't work.");
+        }
     }
 
     private Category mapRow(ResultSet row) throws SQLException
